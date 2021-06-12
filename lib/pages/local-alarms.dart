d65c2d7a074a6e99f80b595afb48490ad88c8bea
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2021-06-12 12:49:56 +0800

    Created a _DayCheckBox widget for Local Alarms.

--- lib/pages/local-alarms.dart
+++ lib/pages/local-alarms.dart
@@ -16,157 +16,6 @@ class LocalAlarmsPage extends StatefulWidget {
   State<StatefulWidget> createState() => _LocalAlarmsPageState();
 }
 
-class LocalAlarmListItem extends StatelessWidget {
-  LocalAlarmListItem({
-    required this.alarm,
-  });
-
-  /// Alarm object associated with this widget
-  final Alarm alarm;
-  /// BLoC for local alarms.
-  final LocalAlarmsBloc _bloc = BlocProvider.localAlarmsBlock;
-  /// Radius for rounded cards.
-  static const double _cardRadius = 32;
-  /// Changes the alarm description accordingly
-  TextEditingController textController() {
-    return TextEditingController(text: alarm.description);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Color _textColor = Theme.of(context).colorScheme.onSurface;
-
-    /// Provides a single day checkbox for a given alarm and day.
-    Widget _getDayCheckbox(Alarm alarm, Days day) {
-      bool _selected = alarm.days.contains(day);
-
-      return
-        InkWell(
-            onTap: () {
-              alarm.days.contains(day)
-                  ? alarm.days.remove(day)
-                  : alarm.days.add(day);
-              _bloc.updateAlarm(alarm);
-            },
-            child: Container(
-              height: 32,
-              width: 32,
-              decoration: BoxDecoration(
-                border: Border.all(
-                    width: .5,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .onSurface
-                        .withAlpha(_selected ? 0 : 100)),
-                shape: BoxShape.circle,
-                color: _selected
-                    ? Theme.of(context).colorScheme.onSurface.withAlpha(100)
-                    : Colors.transparent,
-              ),
-              child: Center(
-                child: Text(
-                  Days.shortStrings[day]!,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context)
-                          .colorScheme
-                          .onSurface
-                          .withAlpha(_selected ? 255 : 100)),
-                ),
-              ),
-            ));
-    }
-
-    return Container(
-      margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-      padding: EdgeInsets.only(left: 24, right: 24, top: 32, bottom: 24),
-      decoration: BoxDecoration(
-          gradient: RadialGradient(
-              center: Alignment(-0.8, -0.5),
-              radius: 3,
-              colors: [Colors.grey[800]!, Colors.grey[900]!]),
-          borderRadius: BorderRadius.all(Radius.circular(_cardRadius)),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                blurRadius: 8,
-                offset: Offset(2, 2))
-          ]),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  child: Text(alarm.time.format(context),
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline3!
-                          .copyWith(color: _textColor)),
-                  onTap: () async {
-                    TimeOfDay? selectedTimeFuture =
-                    await showTimePicker(context: context, initialTime: alarm.time);
-                    if (selectedTimeFuture != null) {
-                      alarm.time = selectedTimeFuture;
-                      _bloc.updateAlarm(alarm);
-                    }},
-                ),
-                Switch(
-                  activeColor: Theme.of(context).primaryColor,
-                  value: alarm.activated,
-                  onChanged: (bool isAlarmActivated) {
-                    alarm.activated = isAlarmActivated;
-                    _bloc.updateAlarm(alarm);
-                    //setState(() {});
-                  },
-                )
-              ],
-            ),
-            Container(
-              height: 36,
-              margin: EdgeInsets.only(top: 8),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: Days.allList
-                      .map((day) => _getDayCheckbox(alarm, day))
-                      .toList()),
-            ),
-            Container(
-              child: TextField(
-                controller: textController(),
-                readOnly: true,
-                onTap: () async {
-                  String? newDescription = await showInputDialog(
-                      context: context,
-                      title: "New Note",
-                      labelText: "Note",
-                      doneAction: "Confirm",
-                      cancelAction: "Cancel");
-
-                  if (newDescription != null) {
-                    alarm.description = newDescription;
-                    _bloc.updateAlarm(
-                        alarm); // Update database without refreshing state
-                  }
-                },
-              ),
-            ),
-            Container(
-                margin: EdgeInsets.only(top: 8),
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    _bloc.deleteAlarm(alarm);
-                  },
-                ))
-          ]),
-    );
-  }
-}
-
 class _LocalAlarmsPageState extends State<LocalAlarmsPage>
     with AutomaticKeepAliveClientMixin {
   @override
@@ -208,7 +57,7 @@ class _LocalAlarmsPageState extends State<LocalAlarmsPage>
                 return ListView.builder(
                   itemCount: alarms.length + 1,
                   itemBuilder: (context, index) => index < alarms.length
-                      ? LocalAlarmListItem(alarm: alarms[index])
+                      ? _LocalAlarmListItem(alarm: alarms[index])
                       : _getAlarmAdder(alarms),
                 );
               } else if (snapshot.hasError) {
@@ -263,6 +112,178 @@ class _LocalAlarmsPageState extends State<LocalAlarmsPage>
                           style: TextStyle(fontSize: 24, color: _color))
                     ],
                   ),
-                ))));
+                )
+            )
+        )
+    );
+  }
+}
+
+class _LocalAlarmListItem extends StatelessWidget {
+  _LocalAlarmListItem({
+    required this.alarm,
+  });
+
+  /// Alarm object associated with this widget
+  final Alarm alarm;
+
+  /// BLoC for local alarms.
+  final LocalAlarmsBloc _bloc = BlocProvider.localAlarmsBlock;
+
+  /// Radius for rounded cards.
+  static const double _cardRadius = 32;
+
+  /// Changes the alarm description accordingly
+  TextEditingController textController() {
+    return TextEditingController(text: alarm.description);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color _textColor = Theme.of(context).colorScheme.onSurface;
+
+    return Container(
+      margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+      padding: EdgeInsets.only(left: 24, right: 24, top: 32, bottom: 24),
+      decoration: BoxDecoration(
+          gradient: RadialGradient(
+              center: Alignment(-0.8, -0.5),
+              radius: 3,
+              colors: [Colors.grey[800]!, Colors.grey[900]!]),
+          borderRadius: BorderRadius.all(Radius.circular(_cardRadius)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 8,
+                offset: Offset(2, 2))
+          ]),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  child: Text(alarm.time.format(context),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline3!
+                          .copyWith(color: _textColor)),
+                  onTap: () async {
+                    TimeOfDay? selectedTimeFuture = await showTimePicker(
+                        context: context, initialTime: alarm.time);
+                    if (selectedTimeFuture != null) {
+                      alarm.time = selectedTimeFuture;
+                      _bloc.updateAlarm(alarm);
+                    }
+                  },
+                ),
+                Switch(
+                  activeColor: Theme.of(context).primaryColor,
+                  value: alarm.activated,
+                  onChanged: (bool isAlarmActivated) {
+                    alarm.activated = isAlarmActivated;
+                    _bloc.updateAlarm(alarm);
+                    //setState(() {});
+                  },
+                )
+              ],
+            ),
+            Container(
+              height: 36,
+              margin: EdgeInsets.only(top: 8),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: Days.allList
+                      .map((day) => _DayCheckBox(alarm: alarm, day: day))
+                      .toList()),
+            ),
+            Container(
+              child: TextField(
+                controller: textController(),
+                readOnly: true,
+                onTap: () async {
+                  String? newDescription = await showInputDialog(
+                      context: context,
+                      title: "New Note",
+                      labelText: "Note",
+                      doneAction: "Confirm",
+                      cancelAction: "Cancel");
+
+                  if (newDescription != null) {
+                    alarm.description = newDescription;
+                    _bloc.updateAlarm(
+                        alarm); // Update database without refreshing state
+                  }
+                },
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 8),
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    _bloc.deleteAlarm(alarm);
+                  },
+                ))
+          ]),
+    );
+  }
+}
+
+/// Single day checkbox for a given alarm and day.
+class _DayCheckBox extends StatelessWidget {
+  _DayCheckBox({required this.alarm, required this.day});
+
+  /// Alarm which this checkbox modifies.
+  final Alarm alarm;
+
+  /// Day which this checkbox modifies.
+  final Days day;
+
+  /// BLoC for local alarms.
+  final LocalAlarmsBloc _bloc = BlocProvider.localAlarmsBlock;
+
+  @override
+  Widget build(BuildContext context) {
+    bool _selected = alarm.days.contains(day);
+
+    return InkWell(
+        onTap: () {
+          alarm.days.contains(day)
+              ? alarm.days.remove(day)
+              : alarm.days.add(day);
+          _bloc.updateAlarm(alarm);
+        },
+        child: Container(
+          height: 32,
+          width: 32,
+          decoration: BoxDecoration(
+            border: Border.all(
+                width: .5,
+                color: Theme.of(context)
+                    .colorScheme
+                    .onSurface
+                    .withAlpha(_selected ? 0 : 100)),
+            shape: BoxShape.circle,
+            color: _selected
+                ? Theme.of(context).colorScheme.onSurface.withAlpha(100)
+                : Colors.transparent,
+          ),
+          child: Center(
+            child: Text(
+              Days.shortStrings[day]!,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context)
+                      .colorScheme
+                      .onSurface
+                      .withAlpha(_selected ? 255 : 100)),
+            ),
+          ),
+        )
+    );
   }
 }
