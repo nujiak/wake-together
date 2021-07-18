ca4532003bebbca39fdf946643d5e842067dff54
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2021-07-19 01:19:02 +0800

    Moved voting related database operations to Cloud Functions.

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable linebreak-style */
 // The Cloud Functions for Firebase SDK to
 // create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
+const {firestore} = require("firebase-admin");
 admin.initializeApp();
 admin.firestore().settings({ignoreUndefinedProperties: true});
 
@@ -28,11 +30,11 @@ const CURRENT_ALARM_FIELD = "currentAlarm";
 
 // Name for the field containing an alarm option's time in
 // /channels/channelId/options/.
-// const TIME_FIELD = "time";
+const TIME_FIELD = "time";
 
 // Name for the field containing an alarm option's vote count in
 // /channels/channelId/options/.
-// const VOTES_FIELD = "votes";
+const VOTES_FIELD = "votes";
 
 // Name for the field containing a boolean representing whether a user has
 // voted in an alarm channel in /users/userId/subscribed_channels/channelId/.
@@ -48,11 +50,11 @@ const SUBSCRIBERS_SUB = "subscribers";
 
 // Name for the sub-collection containing an alarm channel's current
 // voting options in /channels/channelId.
-// const OPTIONS_SUB = "options";
+const OPTIONS_SUB = "options";
 
 // Name for the sub-collection containing an alarm channel's current
 // voting options in /channels/channelId.
-// const VOTES_SUB = "votes";
+const VOTES_SUB = "votes";
 
 // Name for the top-level collection containing all user documents.
 const USERS_COLLECTION = "users";
@@ -61,7 +63,7 @@ const USERS_COLLECTION = "users";
 const CHANNELS_COLLECTION = "channels";
 
 // Adds the alarm channel details to the user when the user is added.
-exports.onAddUserToChannel = functions.firestore
+exports.onAddUserToChannel = functions.region("asia-southeast2").firestore
     .document(`/${CHANNELS_COLLECTION}/{channelId}/${SUBSCRIBERS_SUB}/{userId}`)
     .onCreate((_snapshot, context) => {
       const channelId = context.params.channelId;
@@ -79,4 +81,122 @@ exports.onAddUserToChannel = functions.firestore
                 [CURRENT_ALARM_FIELD]: snapshot.get(CURRENT_ALARM_FIELD),
                 [HAS_VOTED_FIELD]: false,
               }, {merge: true}));
+    });
+
+exports.recountVotes = functions.region("asia-southeast2").firestore
+    .document(`/${CHANNELS_COLLECTION}/{channelId}/${VOTES_SUB}/{userId}`)
+    .onWrite((change, context) => {
+      const channelId = context.params.channelId;
+      const userId = context.params.userId;
+
+      // Check if user voted or opted out.
+      const hasVoted = change.after.get(TIME_FIELD) != undefined;
+
+      // Update user's opt-in status.
+      const updateUserHasVoted = admin.firestore()
+          .collection(USERS_COLLECTION)
+          .doc(userId)
+          .collection(SUBSCRIBED_CHANNELS_SUB)
+          .doc(channelId)
+          .set({[HAS_VOTED_FIELD]: hasVoted}, {merge: true});
+
+      // Fetch all user votes for this alarm channel.
+      const allVotes = updateUserHasVoted.then((_) => {
+        return admin.firestore()
+            .collection(`${CHANNELS_COLLECTION}/${channelId}/${VOTES_SUB}`)
+            .get()
+            .then((snapshot) => snapshot.docs);
+      });
+
+      // Tally all votes.
+      const votesMap = allVotes.then((docSnapshots) => {
+        // Stores the number of votes for each option.
+        const voteCounts = new Map();
+
+        for (let i = 0; i < docSnapshots.length; ++i) {
+          const docSnap = docSnapshots[i];
+          if (docSnap != undefined) {
+            const timestamp = docSnap.get(TIME_FIELD);
+            voteCounts.set(
+                timestamp.toMillis(),
+                voteCounts.has(timestamp.toMillis()) ?
+                voteCounts.get(timestamp.toMillis()) + 1 : 1);
+          }
+        }
+
+        return voteCounts;
+      });
+
+      // Get all vote options.
+      const voteOptions = admin.firestore()
+          .collection(`/${CHANNELS_COLLECTION}/${channelId}/${OPTIONS_SUB}`)
+          .get()
+          .then((querySnapshot) => querySnapshot.docs);
+
+      // Update each option with its vote count.
+      const updateOptions = Promise.all([votesMap, voteOptions])
+          .then((values) => {
+            const map = values[0];
+            const options = values[1];
+
+            options.forEach((optionSnapshot) => {
+              if (optionSnapshot != undefined) {
+                const timestamp = optionSnapshot.get(TIME_FIELD);
+                optionSnapshot.ref.set(
+                    {[VOTES_FIELD]:
+                        map.has(timestamp.toMillis()) ?
+                        map.get(timestamp.toMillis()) : 0},
+                    {merge: true}
+                );
+              }
+            });
+          });
+
+      // Find the highest voted timestamp.
+      const highestVote = votesMap.then((map) => {
+        let highestTimestamp;
+        let highestCount;
+
+        for (const [timestampMillis, count] of map.entries()) {
+          if (highestTimestamp == undefined || count > highestCount) {
+            highestTimestamp = firestore.Timestamp.fromMillis(timestampMillis);
+            highestCount = count;
+          }
+        }
+
+        return highestTimestamp;
+      });
+
+      // Get all channels under the users collection.
+      const userChannels = admin.firestore()
+          .collectionGroup(SUBSCRIBED_CHANNELS_SUB)
+          .where(CHANNEL_ID_FIELD, "==", channelId)
+          .get()
+          .then((snapshot) => snapshot.docs)
+          .then((docSnaps) => docSnaps.map((docSnap) => docSnap.ref));
+
+      // Update info of all channels under the users collection.
+      const updateUserChannels = Promise.all([highestVote, userChannels])
+          .then((values) => {
+            const highestVote = values[0];
+            const userChannels = values[1];
+
+            // Update all channels under users.
+            for (let i = 0; i < userChannels.length; ++i) {
+              const channel = userChannels[i];
+              if (channel != undefined) {
+                channel.set(
+                    {[CURRENT_ALARM_FIELD]: highestVote},
+                    {merge: true}
+                );
+              }
+            }
+
+            // Update alarm channel.
+            admin.firestore()
+                .doc(`${CHANNELS_COLLECTION}/${channelId}`)
+                .set({[CURRENT_ALARM_FIELD]: highestVote}, {merge: true});
+          });
+
+      return Promise.all([updateUserChannels, updateOptions]);
     });
